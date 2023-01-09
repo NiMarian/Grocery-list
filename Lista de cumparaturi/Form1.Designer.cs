5079ab2c5d09bedcee035a39a7542a68fcc9d207
NiMarian <115469165+NiMarian@users.noreply.github.com>
2023-01-10 00:59:53 +0200

    Add files via upload

---
+++ Lista de cumparaturi/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Lista_de_cumparaturi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtObiect = new System.Windows.Forms.TextBox();
+            this.txtPret = new System.Windows.Forms.TextBox();
+            this.lbLista = new System.Windows.Forms.ListBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // txtObiect
+            // 
+            this.txtObiect.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtObiect.Location = new System.Drawing.Point(513, 78);
+            this.txtObiect.Name = "txtObiect";
+            this.txtObiect.Size = new System.Drawing.Size(127, 27);
+            this.txtObiect.TabIndex = 0;
+            // 
+            // txtPret
+            // 
+            this.txtPret.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtPret.Location = new System.Drawing.Point(513, 131);
+            this.txtPret.Name = "txtPret";
+            this.txtPret.Size = new System.Drawing.Size(127, 29);
+            this.txtPret.TabIndex = 1;
+            // 
+            // lbLista
+            // 
+            this.lbLista.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbLista.FormattingEnabled = true;
+            this.lbLista.ItemHeight = 20;
+            this.lbLista.Location = new System.Drawing.Point(199, 78);
+            this.lbLista.Name = "lbLista";
+            this.lbLista.Size = new System.Drawing.Size(295, 124);
+            this.lbLista.TabIndex = 2;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTotal.Location = new System.Drawing.Point(234, 247);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(44, 20);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "Total";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Chartreuse;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.Location = new System.Drawing.Point(199, 346);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 32);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Tomato;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDelete.Location = new System.Drawing.Point(322, 346);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 32);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.OliveDrab;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSave.Location = new System.Drawing.Point(441, 346);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 32);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.Turquoise;
+            this.btnLoad.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLoad.Location = new System.Drawing.Point(565, 346);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 32);
+            this.btnLoad.TabIndex = 7;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(42, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 21);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nicuta Marian";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(42, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 21);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Grupa 610";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(799, 452);
+            this.panel1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lbLista);
+            this.Controls.Add(this.txtPret);
+            this.Controls.Add(this.txtObiect);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtObiect;
+        private TextBox txtPret;
+        private ListBox lbLista;
+        private Label lblTotal;
+        private Button btnAdd;
+        private Button btnDelete;
+        private Button btnSave;
+        private Button btnLoad;
+        private OpenFileDialog openFileDialog1;
+        private Label label1;
+        private Label label2;
+        private Panel panel1;
+    }
+}
---
+++ Lista de cumparaturi/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using System;
+
+namespace Lista_de_cumparaturi
+{
+    public partial class Form1 : Form
+    {
+        // Dicționar pentru a stoca lista de cumpărături
+        Dictionary<string, double> Cumparaturi = new Dictionary<string, double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //  Funcție de adăugare a unui articol în listă
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Obținem numele și prețul articolului din casetele de text
+            string obiect = txtObiect.Text;
+            double pret;
+
+            // Validăm introducerea prețului
+            if (!double.TryParse(txtPret.Text, out pret) || pret <= 0)
+            {
+                MessageBox.Show("Vă rugăm să introduceți un preț valid");
+                return;
+            }
+
+            // Adăugăm articolul în listă
+            if (!Cumparaturi.ContainsKey(obiect))
+            {
+               
+                Cumparaturi.Add(obiect, pret);
+               
+                UpdateListBox();
+                
+            }
+            else
+            {
+                MessageBox.Show("Elementul există deja în listă");
+            }
+           
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Obținem elementul selectat din caseta de listă
+            string selectedItem = lbLista.SelectedItem as string;
+
+            // Ne asigurăm că este selectat un articol
+            if (selectedItem == null)
+            {
+                MessageBox.Show("Please select an item to delete.");
+                return;
+            }
+
+            // Obținem numele articolului împărțind șirul la două puncte
+            string itemName = selectedItem.Split(':')[0];
+
+            // Eliminăm elementul din dicționar
+            Cumparaturi.Remove(itemName);
+
+            // Actualizăm caseta de listă și eticheta totală
+            UpdateListBox();
+        }
+
+        private void UpdateListBox()
+        {
+            lbLista.Items.Clear();
+
+            // Adăugăm fiecare articol din listă în caseta de listă
+            foreach (KeyValuePair<string, double> obiect in Cumparaturi)
+            {
+                lbLista.Items.Add($"{obiect.Key}: ${obiect.Value}");
+            }
+
+            // Calculăm și afișăm costul total
+            double totalCost = Cumparaturi.Values.Sum();
+            lblTotal.Text = $"Total: ${totalCost}";
+        }
+
+        // Funcție de salvare a listei într-un fișier .txt
+        void btnSave_Click(object sender, EventArgs e)
+        {
+            // Afișăm dialogul de salvare a fișierului
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Text file|*.txt";
+            saveFileDialog1.Title = "Save a Text File";
+            saveFileDialog1.ShowDialog();
+
+            // Dacă calea fișierului nu este un string gol, deschidem pentru salvare
+            if (saveFileDialog1.FileName != "")
+            {
+                // Creăm un string builder pentru a stoca liniile de text
+                StringBuilder sb = new StringBuilder();
+
+                // Iterăm prin dicționar și adăugăm fiecare articol și preț la string builder
+                foreach (KeyValuePair<string, double> item in Cumparaturi)
+                {
+                    sb.AppendLine($"{item.Key}: ${item.Value}");
+                }
+
+                // Scriem string builder în fișier
+                File.WriteAllText(saveFileDialog1.FileName, sb.ToString());
+            }
+        }
+
+
+        // Funcție pentru a încărca lista dintr-un fișier .txt
+     
+        void btnLoad_Click(object sender, EventArgs e)
+        {
+            // Afișăm dialogul de deschidere a fișierului
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Obținem calea fișierului selectat
+                string filePath = openFileDialog1.FileName;
+
+                // Citim conținutul fișierului
+                string text = File.ReadAllText(filePath);
+
+                // Împărțim textul în rânduri
+                string[] lines = text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+                // Analizăm rândurile și adăugați articolele și prețurile în dicționar
+                foreach (string line in lines)
+                {
+                    // Împărțim linia în părți
+                    string[] parts = line.Split(':');
+
+                    // Printăm părțile
+                    Console.WriteLine($"Line: {line}");
+                    Console.WriteLine($"Parts: {string.Join(", ", parts)}");
+
+                    // Verificăm dacă există suficiente părți
+                    if (parts.Length >= 2)
+                    {
+                        // Tăiăm spațiul alb din părțile și analizăm prețul
+                        string item = parts[0].Trim();
+                        double price = double.Parse(parts[1].Trim().Substring(1));
+
+                        // Adăugăm articolul și prețul în dicționar
+                        Cumparaturi.Add(item, price);
+                    }
+                }
+
+                // Iterăm prin dicționar și printăm articolele și prețurile
+                foreach (KeyValuePair<string, double> item in Cumparaturi)
+                {
+                    Console.WriteLine($"{item.Key}: ${item.Value}");
+                }
+
+                // Actualizăm caseta cu listă
+                UpdateListBox();
+            }
+        }
+
+    }
+    
+}
+  
